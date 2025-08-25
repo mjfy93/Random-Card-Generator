ab894c760f0769e97376f039ff339d2136df9242
mjfy93 <maria.jose.ferro@hotmail.com>
2025-08-25 23:24:39 +0000

    Second draft

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-import "bootstrap";
-import "./style.css";
+myButton.onclick = function () {
+    let suits = ["♦", "♥", "♠", "♣"]
+    let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+    let newSuit = suits[Math.floor(Math.random() * suits.length)];
+    let newNumber = numbers[Math.floor(Math.random() * numbers.length)];
+
+    document.getElementById("topSuit").innerHTML
+     = newSuit;
+    document.getElementById("cardNumber").innerHTML = newNumber;
+    document.getElementById("bottomSuit").innerHTML = newSuit;
+
+    if (newSuit ==="♠" || newSuit ==="♣"){
+       document.getElementById("topSuit").style.color = "black";
+        document.getElementById("bottomSuit").style.color = "black";
+    
+    }else {
+        document.getElementById("topSuit").style.color = "red";
+        document.getElementById("bottomSuit").style.color = "red";
+    }
+}
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
